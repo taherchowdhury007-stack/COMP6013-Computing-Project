984337332666249f6e33e7cb246b58032737285b
taherchowdhury007-stack <taherchowdhury007@gmail.com>
2026-02-13 14:22:41 +0000

    Added backtesting engine placeholder (engine.py)

---
+++ src/src/models/src/models/src/backtesting/src/backtesting/engine.py
@@ -0,0 +1,12 @@
+"""
+Backtesting engine placeholder.
+
+Responsible for:
+- iterating over time
+- applying strategy signals
+- simulating trades
+- tracking portfolio value
+"""
+
+def run_backtest():
+    pass
