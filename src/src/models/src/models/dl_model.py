573414acdbb0cd3adce8b43e7ad82ce7b741a9d9
taherchowdhury007-stack <taherchowdhury007@gmail.com>
2026-02-13 14:21:57 +0000

    Added DL model module interface (dl_model.py)

---
+++ src/src/models/src/models/dl_model.py
@@ -0,0 +1,16 @@
+"""
+Deep Learning model module (LSTM/CNN placeholder).
+
+Interface:
+- train(train_sequences, y_train)
+- predict(test_sequences)
+"""
+
+class DLModel:
+    def train(self, train_sequences, y_train):
+        """Train the DL model."""
+        pass
+
+    def predict(self, test_sequences):
+        """Return predictions for test sequences."""
+        pass
