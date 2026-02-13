4e47a0f3cc18d63ad859cdc5f10e0ed94fed26c9
taherchowdhury007-stack <taherchowdhury007@gmail.com>
2026-02-13 14:21:29 +0000

    Added ML model module interface (ml_model.py)

---
+++ src/src/models/ml_model.py
@@ -0,0 +1,16 @@
+"""
+Machine Learning model module (XGBoost placeholder).
+
+Interface:
+- train(X_train, y_train)
+- predict(X_test)
+"""
+
+class MLModel:
+    def train(self, X_train, y_train):
+        """Train the ML model."""
+        pass
+
+    def predict(self, X_test):
+        """Return predictions for X_test."""
+        pass
